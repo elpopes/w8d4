54c427ddcfb80d30a0b34ee0f45fdd12412ce70f
lorenzo <lorenzotijerina@gmail.com>
2023-01-05 15:37:33 -0500

    complete clock

---
+++ callbacks_ex.js
@@ -0,0 +1,45 @@
+class Clock {
+  constructor() {
+    // 1. Create a Date object.
+    const now = new Date();
+    // 2. Store the hours, minutes, and seconds.
+    this.hours = now.getHours();
+    this.minutes = now.getMinutes();
+    this.seconds = now.getSeconds();
+    // 3. Call printTime.
+    // printTime(hours, minutes, seconds);
+    // 4. Schedule the tick at 1 second intervals.
+    setInterval(this._tick.bind(this), 1000);
+  }
+
+  printTime() {
+    // Format the time in HH:MM:SS
+    const time = `${this.hours}:${this.minutes}:${this.seconds}`;
+
+    // Use console.log to print it.
+    console.log(time);
+  }
+
+  _tick() {
+    // 1. Increment the time by one second.
+    this.seconds += 1;
+
+    if (this.seconds === 60) {
+      this.minutes += 1;
+      this.seconds = 0;
+    }
+
+    if (this.minutes === 60) {
+      this.minutes = 0;
+      this.hours += 1;
+    }
+
+    if (this.hours === 24) {
+      this.hours = 0;
+    }
+    // 2. Call printTime.
+    this.printTime();
+  }
+}
+
+const clock = new Clock();
